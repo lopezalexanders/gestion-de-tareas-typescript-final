ad8e10206b0b37186489b7c840f9f8d38ddab0ca
Martin Alexanders Lopez Mamani <lopezalexanders@gmail.com>
2025-11-09 01:44:00 -0400

    Delete backend directory

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import { logger } from './observability/logger.js';
-
-const port = Number(process.env.PORT ?? 3000);
-
-app.listen(port, () => {
-  logger.info({ port }, 'DevPro Tasks API started');
-});
